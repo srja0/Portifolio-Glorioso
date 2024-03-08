550f2002b4b6b90ecf8a53952292ddd54c64ec00
srja0 <125494645+srja0@users.noreply.github.com>
2024-03-07 21:04:57 -0300

    first commit

--- portifolio/src/componentes/section/Footer.js
+++ portifolio/src/componentes/section/Footer.js
@@ -1,7 +1,17 @@
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
+import styles from './Footer.module.css'
+
+
 function Footer(){
     return (
-        <div>
-            <h1> Aqui ficará Roda pé </h1>
+        <div className={styles.footer}>
+            <ul>
+                <li> <a href='https://www.instagram.com/sr__ja0/'> <FaInstagram size={30}/> </a></li>
+                <li> <a href='https://www.linkedin.com/in/jo%C3%A3o-victor-gon%C3%A7alves-correia/'> <FaLinkedin size={30}/> </a></li>
+                <li> <a href='https://github.com/srja0'><FaGithub size={30}/> </a></li>                                
+            </ul>
+            <p>joaogoncalves6270@gmail.com</p>
+            <p>João Gonçalves © 2024</p>
         </div>
     )
 }
